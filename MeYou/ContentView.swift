49c6966563e573d9fa4c76cb1127e7c342fca2da
hjaeho95 <hjaeho95>
2021-07-02 15:14:33 +0900

    Initial Commit

---
+++ MeYou/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MeYou
+//
+//  Created by rabbit on 2021/07/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MeYou/MeYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeYouApp.swift
+//  MeYou
+//
+//  Created by rabbit on 2021/07/02.
+//
+
+import SwiftUI
+
+@main
+struct MeYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
